5ce77573e9dcf8fb2d79f0063422a6c115246f86
Pavel <khurorraks@gmail.com>
2021-08-04 21:43:42 +0300

    captcha for Login page

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import {requestUsers} from "../redux/users-reducer";
 
 const instance = axios.create({
   baseURL: 'https://social-network.samuraijs.com/api/1.0/',
@@ -55,10 +56,16 @@ export const authAPI = {
   me() {
     return instance.get(`auth/me`)
   },
-  login(email, password, rememberMe = false) {
-    return instance.post(`auth/login`, { email, password, rememberMe })
+  login(email, password, rememberMe = false, captcha = null) {
+    return instance.post(`auth/login`, { email, password, rememberMe, captcha })
   },
   logout() {
     return instance.delete(`auth/login`)
+  }
+}
+
+export const securityAPI = {
+  getCaptchaUrl() {
+    return instance.get('security/get-captcha-url')
   }
 }
