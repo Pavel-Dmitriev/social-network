880976a305f0e550e1fd0f2ee0249118d7ec63be
pdmitriev <khurorraks@gmail.com>
2025-11-26 01:13:58 +0300

    feat: подключить шрифт GopherMono

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import postcssNested from "postcss-nested";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    open: "/",
     port: 3000,
+    // warmup: {
+    //   clientFiles: ["./src/index.css"],
+    // },
+  },
+  css: {
+    postcss: {
+      plugins: [postcssNested],
+    },
   },
   build: {
     outDir: "build",
