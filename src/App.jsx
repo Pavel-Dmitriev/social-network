fdccc34481184f93614f0f53a89c3a80382652ca
pdmitriev <khurorraks@gmail.com>
2025-12-10 16:55:51 +0300

    feat: подключить typescript + поменять шрифт на бесплатный

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ const App = () => {
       <main className="min-h-125 -translate-y-8 relative max-w-289.75 mx-auto rounded-b-4xl bg-white/20 backdrop-blur-[20px] px-6 pb-8 pt-16">
         <Outlet />
       </main>
-      <footer className="bg-backdrop h-full"></footer>
     </>
   );
 };
